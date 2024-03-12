b09137eab4d3a317e7d6c5576566cebad188e45e
unknown <ishtuuabbas786@gmail.com>
2024-03-12 15:21:48 +0500

    first commet

---
+++ project-1/src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import IsSection from "./components/Section";
+import Navigation from "./components/Navigation";
+const App = () => {
+  return (
+    <div>
+      <Navigation />
+      <IsSection />
+    </div>
+  );
+};
+
+export default App;
---
+++ project-1/src/components/Section.jsx
@@ -0,0 +1,33 @@
+const IsSection = () => {
+  return (
+    <main className="is container">
+      <div className="is-content">
+        <h1>YOUR FEET DESERVE THE BEST</h1>
+        <p>
+          YOUR FEET DESERVE THE BEST AND WE’RE HERE TO HELP YOU WITH OUR
+          SHOES.YOUR FEET DESERVE THE BEST AND WE’RE HERE TO HELP YOU WITH OUR
+          SHOES.
+        </p>
+
+        <div className="is-btn">
+          <button>Shop Now </button>
+          <button className="secondary-btn">Category</button>
+        </div>
+
+        <div className="shopping">
+          <p>Also Available On</p>
+
+          <div className="brand-icons">
+            <img src="/images/amazon.png" alt="amazon-logo" />
+            <img src="/images/flipkart.png" alt="flipkart-logo" />
+          </div>
+        </div>
+      </div>
+      <div className="is-image">
+        <img src="/images/is-image.png" alt="is-image" />
+      </div>
+    </main>
+  );
+};
+
+export default IsSection;
